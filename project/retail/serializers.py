ce30736d940a4771938f2e9d3a4f7eaff00ff83d
bsmukasa <bsmukasa@gmail.com>
2016-01-26 14:44:53 -0500

    Add ChainSerializer class with create and update methods.

---
+++ project/retail/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+
+from project.retail.models import Chain, Store
+
+
+class ChainSerializer(serializers.Serializer):
+    pk = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(required=False, allow_blank=True, max_length=100)
+    description = serializers.CharField(required=False, allow_blank=True, max_length=1000)
+    slogan = serializers.CharField(required=False, allow_blank=True, max_length=500)
+    founded_date = serializers.DateField()
+    website = serializers.URLField()
+
+    def create(self, validated_data):
+        """Create and return a new Chain instance given the validated data.
+
+        :param validated_data: The validated data used to create a Chain instance.
+        :return: Newly created Chain instance.
+        """
+        return Chain.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update and return an existing Chain instance, given the validated data.
+
+        :param instance: A Chain instance.
+        :param validated_data: The validated data used to update a Chain instance.
+        :return: Updated Chain instance.
+        """
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.slogan = validated_data.get('slogan', instance.slogan)
+        instance.founded_date = validated_data.get('founded_date', instance.founded_date)
+        instance.website = validated_data.get('website', instance.website)
+        instance.save()
+        return instance
